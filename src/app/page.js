aa2a198f6cb4bb85aa4c0befe568e357cd144dcf
sbectorProd <sbector.prod@gmail.com>
2025-02-09 00:47:19 -0600

    Clean r3f app

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+'use client'
+
+import { Canvas } from "@react-three/fiber"
+
+export default function Home() {
+  return (
+    <>
+    <Canvas>
+            <mesh>
+                <torusKnotGeometry />
+                <meshNormalMaterial />
+            </mesh>
+        </Canvas>
+    </>
+  );
+}
