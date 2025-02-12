93401ed46705bc9e78ffe3348a5b689b571ada40
sbectorProd <sbector.prod@gmail.com>
2025-02-11 21:06:48 -0600

    Berg model test

--- src/app/_components/Experience.js
+++ src/app/_components/Experience.js
@@ -1,58 +1,31 @@
-import { OrbitControls } from "@react-three/drei";
-import { button, useControls } from "leva";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import { Suspense } from "react";
 
 export default function Experience() {
-
-    const {perfVisible} = useControls({
-        perfVisible: true
-    })
-
-    const { position, color, visible, clickMe, choice } = useControls('sphere',{
-        position: {
-            value: { x: -2, y: 0},
-            step: 0.01,
-            joystick: 'invertY'
-        },
-        color: '#ff0000',
-        visible: true,
-        clickMe: button(()=>{
-            console.log('clicked')
-        }),
-        choice: {options: ['a', 'b', 'c']}
-    })
-    
-    const { scale } = useControls('cube',{
-        scale: {
-            value: 1.7,
-            step: 0.01,
-            min: 0,
-            max: 5
-        }
-    })
+    const url = "/models/Berg.glb";
+    const gltf = useGLTF(url);
+    console.log(gltf);
 
     return (
         <>
-            { perfVisible ? <Perf position="top-left"/>: null}
+            <Perf position="top-left" />
             <OrbitControls makeDefault />
-            <directionalLight position={[1, 2, 3]} intensity={2.5} />
+            <directionalLight castShadow position={[1, 2, 3]} intensity={2.5} />
             <ambientLight />
-            <group >
-                <mesh position-x={2.5} scale={scale}>
-                    <boxGeometry />
-                    <meshStandardMaterial color='mediumPurple' />
-                </mesh>
 
-                <mesh position={[position.x, position.y, 0]} scale={0.7} visible={visible}>
-                    <sphereGeometry args={[1.5, 32, 32]} />
-                    <meshStandardMaterial color={ color } />
-                </mesh>
-            </group>
-
-            <mesh rotation-x={Math.PI * -0.5} position-y={-2} scale={10}>
+            <mesh recieveShadow rotation-x={Math.PI * -0.5} position-y={-2} scale={10}>
                 <planeGeometry />
                 <meshStandardMaterial color='greenyellow' />
             </mesh>
+            <Suspense>
+                <primitive
+
+                    object={gltf.scene}
+                    position={[0, -2, 0]}
+                    scale={0.5}
+                />
+            </Suspense>
         </>
     )
 }
